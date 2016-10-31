c67b89748948e8c201cf512388f53a245c54c7bb
Daniel Sjösten <dansjo_87@hotmail.com>
2016-10-31 15:12:23 +0100

    Adding a sysout on line 4

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		System.out.println("This line i added from the dev branch");
 		System.out.println("This line was added from the master braanch");
 		System.out.println("I added this line from the dev-branch. Just to have something to merge.");
+		System.out.println("Adding line 4");
 	}
 
 }
