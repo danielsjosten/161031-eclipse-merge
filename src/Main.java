c1a58f590c7902383302f042a0f289b454c4cbbd
Daniel Sjösten <dansjo_87@hotmail.com>
2016-10-31 14:48:36 +0100

    Commit from master, added a new sysout in the main mathod

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("This line i added from the dev branch");
+		System.out.println("This line was added from the master braanch");
 	}
 
 }
