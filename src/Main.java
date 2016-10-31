4cf233e2e0d0b2e8e4fa11a6c5fd410aac051bab
Daniel Sjösten <dansjo_87@hotmail.com>
2016-10-31 14:59:07 +0100

    I added a new sysout in the main method so that i can merge it with the master branch

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("This line i added from the dev branch");
 		System.out.println("This line was added from the master braanch");
+		System.out.println("I added this line from the dev-branch. Just to have something to merge.");
 	}
 
 }
